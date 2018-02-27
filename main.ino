e9e76dc235d432cf40226b9cb8740966ab570b83
Gordon Sun <gsun1729@gmail.com>
2018-02-26 23:53:42 -0500

    restructuring directories

---
+++ main.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+Servo servo;
+
+int photodiode = 0;
+int Servo_control = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(Servo_control)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
